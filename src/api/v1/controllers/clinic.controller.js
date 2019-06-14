460d0e7b63ee4d2b0bd4f7dfebc3a2792cd5e512
Mohamed Essam <eng.mohamed3ssam@gmail.com>
2019-06-14 08:41:28 +0200

    add specialities functionality

--- src/api/v1/controllers/clinic.controller.js
+++ src/api/v1/controllers/clinic.controller.js
@@ -1,11 +1,16 @@
 const Clinic = require('./../models/clinic.model');
 
 module.exports.create = async (req, res, next) => {
-  const { name, address, phone } = req.body;
-  const payload = {
-    name, address, phone,
-  };
-  const clinic = new Clinic(payload);
-  await clinic.save();
-  res.json(clinic);
+  try {
+    const { name, address, phone } = req.body;
+    const payload = {
+      name, address, phone,
+    };
+    const clinic = new Clinic(payload);
+    await clinic.save();
+    res.json(clinic);
+  } catch (error) {
+
+  }
+
 };
